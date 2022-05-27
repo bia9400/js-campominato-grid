d8f64716f313b596d3928061ff0b7fcb55233094
bia9400 <ferro.biagio@gmail.com>
2022-05-27 16:07:08 +0200

    refresh button bugfix

--- Bonus/appBonus.js
+++ Bonus/appBonus.js
@@ -1,27 +1,31 @@
 const gridContainerBonus = document.querySelector(".grid-container");
-const refreshButton=document.getElementById("refresh-button");
+const refreshButton = document.getElementById("refresh-button");
 
 
 
 function createGrid(numeroOrizontale, numeroVerticale) {
     const numeroTotale = numeroOrizontale * numeroVerticale
     for (let i = 1; i <= numeroTotale; i++) {
-        const elementoHtml=document.createElement("div")
+        const elementoHtml = document.createElement("div")
         elementoHtml.classList.add("number-container")
-        elementoHtml.innerHTML=`<h6> ${i} </h6>`
+        elementoHtml.innerHTML = `<h6> ${i} </h6>`
         gridContainerBonus.append(elementoHtml)
-        elementoHtml.addEventListener("click", function(){
-           let x=this;
-           x.classList.add("add-color-onClick")
+        elementoHtml.addEventListener("click", function () {
+            let x = this;
+            x.classList.add("add-color-onClick")
         })
+
+        
+            refreshButton.addEventListener("click", function(){
+                elementoHtml.classList.remove("add-color-onClick")
+            })
+        
     }
 }
 
 
 
 
-function refreshMe(){
-   
-}
-refreshButton.addEventListener("click", refreshMe)
-createGrid(10,10)
+
+
+createGrid(10, 10)
